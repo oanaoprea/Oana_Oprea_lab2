26d1a56876a8780208cfa240f1943beae55d9945
oana oprea <oanamaria.oprea@yahoo.com>
2022-11-15 18:32:52 +0200

    ok

---
+++ Models/LibraryViewModels/PublishedBookData.cs
@@ -0,0 +1,9 @@
+﻿namespace Oana_Oprea_lab2.Models.LibraryViewModels
+{
+    public class PublishedBookData
+    {
+        public int BookID { get; set; }
+        public string Title { get; set; }
+        public bool IsPublished { get; set; }
+    }
+}
